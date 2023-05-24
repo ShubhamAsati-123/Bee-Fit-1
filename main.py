760513d3c610e483fee43f411716cf7d4211acc3
Ashmit Jagtap <ashmitjagtap2209@gmail.com>
2023-05-24 23:35:55 +0530

    removal of readmore page

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ SECRET_key = os.environ.get("SECRET_KEY")
 app.secret_key = SECRET_key
 app.permanent_session_lifetime = timedelta(days=10)
 
-# Dara base inistialization
-db = SQLAlchemy(app=app)
-
 # declaration of all the csv coloumns
 fields = ["ID","User Name","Email_id","Phone_number","Password","Salt","First_login"]
 fields_details = ["ID","age","weight","height","blood group"]
@@ -497,15 +494,7 @@ def takeinput():
 
 
 # read more page route where the full article will be visible
-@app.route('/readmore')
-def readmore():
-    if 'Id' in session:
-        article = session['article']
-        head = articles.read_heading(article)
-        body = articles.read_body(article)
-        return render_template('readmore.html',head=head,body=body)
-    else:
-        return redirect('SignIn_page')
+
 
 # signup or create account page
 @app.route("/signuppage",methods=['GET','POST'])
